bd05ac831c8a087859d8275364f79cda914cb1ed
shehbaz <shehbaz@walkover.in>
2019-08-21 11:40:16 +0530

    routing improvements

--- apps/web-giddh-admin/src/app/app.routes.ts
+++ apps/web-giddh-admin/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { NotFoundComponent } from './404/404-component';
 
 
 export const ROUTES: Routes = [
-  { path: '', loadChildren: './subscription/subscription.module#SubscriptionModule' },
+  { path: 'subscription', loadChildren: './subscription/subscription.module#SubscriptionModule' },
+  { path: 'licence-key', loadChildren: './licence-key/licence-key.module#licenceKeyModule' },
   {path: '404', component: NotFoundComponent},
   {path: '**', pathMatch: 'full', component: NotFoundComponent},
 ];
