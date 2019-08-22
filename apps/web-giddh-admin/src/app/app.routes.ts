306b0bac69983bbc0655edb3ce0fd8ffc40252af
shehbaz <shehbaz@walkover.in>
2019-08-22 15:52:59 +0530

    chnages for routing in login

--- apps/web-giddh-admin/src/app/app.routes.ts
+++ apps/web-giddh-admin/src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './404/404-component';
+import { AdminComponent } from './admin.component';
 
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'subscription', loadChildren: './subscription/subscription.module#SubscriptionModule' },
-  { path: 'licence-key', loadChildren: './licence-key/licence-key.module#licenceKeyModule' },
-  { path: '404', component: NotFoundComponent },
   { path: 'login', loadChildren: './login/login.module#LoginModule' },
+  { path: 'admin', component: AdminComponent,
+    children: [
+      { path: 'subscription', loadChildren: './subscription/subscription.module#SubscriptionModule' },
+      { path: 'licence-key', loadChildren: './licence-key/licence-key.module#licenceKeyModule' },
+      { path: '404', component: NotFoundComponent }
+    ]
+  },
   { path: '**', pathMatch: 'full', component: NotFoundComponent },
 
 ];
