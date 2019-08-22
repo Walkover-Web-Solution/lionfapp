fc3db3bee04f905fb0f001abd188710b7e410abc
shalinee <shalinee@msg91.com>
2019-08-22 18:01:16 +0530

    deisgn modal popup

--- apps/web-giddh-admin/src/app/subscription/components/suscription-container/suscription-container.component.ts
+++ apps/web-giddh-admin/src/app/subscription/components/suscription-container/suscription-container.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { SubscriptionService } from '../../../services/subscription.service';
 import { AppState } from '../../../store';
 import { Store } from '@ngrx/store';
 import { AdminActions } from '../../../actions/admin.actions';
 import { takeUntil, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
 
 @Component({
   selector: 'app-suscription-container',
@@ -12,9 +14,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./suscription-container.component.scss']
 })
 export class SuscriptionContainerComponent implements OnInit {
+  modalRef: BsModalRef;
   private destroyed$: Observable<any>;
   public subscriptionData = [];
-  constructor(private store: Store<AppState>, private adminActions: AdminActions,
+  constructor(private store: Store<AppState>, private adminActions: AdminActions, private modalService: BsModalService,
     private subscriptionService: SubscriptionService) {
     subscriptionService.getAllSubscriptions().subscribe(res => {
       if (res.status === 'success') {
@@ -22,6 +25,16 @@ export class SuscriptionContainerComponent implements OnInit {
       }
     });
   }
+
+
+
+  openModalWithClass(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: 'edit-subscriber' })
+    );
+  }
+
 
   public rightToggle: boolean = false;
   public RightSlide() {
