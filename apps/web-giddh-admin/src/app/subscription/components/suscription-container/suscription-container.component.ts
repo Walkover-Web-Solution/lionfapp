e96f6d281ec03b3f8b11a08c5abdd89366b7c913
sadik-walkover <sadik@walkover.in>
2019-09-11 13:10:59 +0530

    sadique | subscription list

--- apps/web-giddh-admin/src/app/subscription/components/suscription-container/suscription-container.component.ts
+++ apps/web-giddh-admin/src/app/subscription/components/suscription-container/suscription-container.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
 import { SubscriptionService } from '../../../services/subscription.service';
 import { AppState } from '../../../store';
 import { Store } from '@ngrx/store';
@@ -12,10 +14,14 @@ import { Observable } from 'rxjs';
   styleUrls: ['./suscription-container.component.scss']
 })
 export class SuscriptionContainerComponent implements OnInit {
+
+  modalRef: BsModalRef;
+  
+
   private destroyed$: Observable<any>;
   public subscriptionData = [];
   constructor(private store: Store<AppState>, private adminActions: AdminActions,
-    private subscriptionService: SubscriptionService) {
+    private subscriptionService: SubscriptionService, private modalService: BsModalService) {
     subscriptionService.getAllSubscriptions().subscribe(res => {
       if (res.status === 'success') {
         this.subscriptionData = res.body.results;
@@ -28,7 +34,13 @@ export class SuscriptionContainerComponent implements OnInit {
     this.rightToggle = !this.rightToggle;
   }
 
-  ngOnInit() {
+  openModalWithClass(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: 'gray modal-lg' })
+    );
   }
 
+  ngOnInit() {
+  }
 }
--- apps/web-giddh-admin/src/app/subscription/subscription.module.ts
+++ apps/web-giddh-admin/src/app/subscription/subscription.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { SubscriptionRoutingModule } from './subscription-routing.module';
 import { SubscriptionComponent } from './subscription-component/subscription.component';
@@ -9,16 +10,20 @@ import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { BsDatepickerModule } from 'ngx-bootstrap';
 import { AdvanceSearchComponent } from './components/advance-search/advance-search.component';
+import { SuscriptionModalComponent } from './components/suscription-modal/suscription-modal.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 
 
 @NgModule({
-  declarations: [SubscriptionComponent, SuscriptionContainerComponent, UserListComponent, AdvanceSearchComponent],
+  declarations: [SubscriptionComponent, SuscriptionContainerComponent, UserListComponent, AdvanceSearchComponent, SuscriptionModalComponent],
   imports: [
     CommonModule,
     SubscriptionRoutingModule,
     PerfectScrollbarModule,
+    TabsModule,
     BsDatepickerModule.forRoot(),
+    ModalModule.forRoot()
   ]
 })
 export class SubscriptionModule { }
