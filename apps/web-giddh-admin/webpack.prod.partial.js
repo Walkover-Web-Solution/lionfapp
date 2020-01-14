f39edf75727c918f9a1ddf15f46cad187c667b5d
ravindersingh <ravinder@walkover.in>
2020-01-14 15:24:09 +0530

    Updated url in webpack files

--- apps/web-giddh-admin/webpack.prod.partial.js
+++ apps/web-giddh-admin/webpack.prod.partial.js
@@ -1,25 +1,24 @@
 const webpack = require('webpack');
-console.log('demo ------Prod ....................');
 module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      "VERSION": JSON.stringify("4711"),
-      'ENV': JSON.stringify('production'),
-      'isElectron': JSON.stringify(false),
-      'errlyticsNeeded': JSON.stringify(true),
-      'errlyticsKey': JSON.stringify('eTrTpSiedQC4tLUYVDup3RJpc_wFL2QhCaIc0vzpsQA'),
-      'AppUrl': JSON.stringify('https://giddh.com/'),
-      'ApiUrl': JSON.stringify('https://api.giddh.com/'),
-      'APP_FOLDER': JSON.stringify('vulture/'),
-      'process.env.ENV': 'production',
-      'process.env.NODE_ENV': 'production',
-      'process.env.isElectron': JSON.stringify(false),
-      'process.env.errlyticsNeeded': JSON.stringify(true),
-      'process.env.errlyticsKey': JSON.stringify('eTrTpSiedQC4tLUYVDup3RJpc_wFL2QhCaIc0vzpsQA'),
-      'process.env.AppUrl': JSON.stringify('https://giddh.com/'),
-      'process.env.ApiUrl': JSON.stringify('https://api.giddh.com/'),
-      'process.env.APP_FOLDER': JSON.stringify('vulture/')
-    }),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  ]
+    plugins: [
+        new webpack.DefinePlugin({
+            "VERSION": JSON.stringify("4711"),
+            'ENV': JSON.stringify('production'),
+            'isElectron': JSON.stringify(false),
+            'errlyticsNeeded': JSON.stringify(true),
+            'errlyticsKey': JSON.stringify('eTrTpSiedQC4tLUYVDup3RJpc_wFL2QhCaIc0vzpsQA'),
+            'AppUrl': JSON.stringify('https://vulture.giddh.com/'),
+            'ApiUrl': JSON.stringify('https://api.giddh.com/'),
+            'APP_FOLDER': JSON.stringify(''),
+            'process.env.ENV': 'production',
+            'process.env.NODE_ENV': 'production',
+            'process.env.isElectron': JSON.stringify(false),
+            'process.env.errlyticsNeeded': JSON.stringify(true),
+            'process.env.errlyticsKey': JSON.stringify('eTrTpSiedQC4tLUYVDup3RJpc_wFL2QhCaIc0vzpsQA'),
+            'process.env.AppUrl': JSON.stringify('https://vulture.giddh.com/'),
+            'process.env.ApiUrl': JSON.stringify('https://api.giddh.com/'),
+            'process.env.APP_FOLDER': JSON.stringify('')
+        }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ]
 }
