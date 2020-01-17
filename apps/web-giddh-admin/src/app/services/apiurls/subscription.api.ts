35ce9c97c1fec3eb18f14d3557704cdc375a1cdd
rohit-walkover <rohit@walkover.in>
2020-01-17 16:50:20 +0530

    audit log api changed

--- apps/web-giddh-admin/src/app/services/apiurls/subscription.api.ts
+++ apps/web-giddh-admin/src/app/services/apiurls/subscription.api.ts
@@ -4,6 +4,6 @@ export const SUBSCRIPTION_API = {
     GET_USER_SUBSCRIPTION: 'v2/admin/subscriptions/all-by-user?sortBy=:sortBy&sortType=:sortType&page=:page&count=:count',
     GET_SUBSCRIPTION_TOTAL: 'v2/admin/subscription-total',
     GET_ALL_COMPANIES_BY_SUBSCRIPTION_ID: 'v2/admin/subscription/companies?subscriptionId=:subscriptionId',
-    GET_AUDIT_LOGS: 'v2/admin/audit-logs?subscriptionId=:subscriptionId',
+    GET_AUDIT_LOGS: 'v2/admin/audit-logs',
     UPDATE_SUBSCRIPTION: 'v2/admin/subscription?subscriptionId=:subscriptionId'
 };
