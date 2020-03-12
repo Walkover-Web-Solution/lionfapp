f53c0711978d711751bfabf7a4cf62d93507b5fd
sadik-walkover <sadik@walkover.in>
2020-03-12 15:47:29 +0530

    design Update Transactions

--- apps/web-giddh-admin/src/app/subscription/components/update-transactions/update-transactions.component.ts
+++ apps/web-giddh-admin/src/app/subscription/components/update-transactions/update-transactions.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit} from '@angular/core';
-import { PlansService } from '../../../services/plan.service';
 import { IOption } from '../../../theme/ng-select/ng-select';
 @Component({
     selector: 'app-update-transactions',
@@ -11,14 +10,7 @@ export class UpdateTransactionsComponent implements OnInit {
   public createPlanRequest: any = {};
   public durationUnits: IOption[] = [{ label: 'YEAR', value: 'YEAR' }, { label: 'MONTH', value: 'MONTH' }, { label: 'WEEK', value: 'WEEK' }, { label: 'DAY', value: 'DAY' }];
 
-    constructor(private plansService: PlansService) { }
-
-    /**
-     * This function is used to get all allowed onboarding countries
-     *
-     * @memberof CreatePlansComponent
-     */
-
+    constructor() { }
 
     ngOnInit() {
     }
