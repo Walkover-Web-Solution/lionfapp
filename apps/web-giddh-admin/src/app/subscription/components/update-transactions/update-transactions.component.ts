679afe7b3b8490048495b66d4154862f263de83e
Ravinder <ravindersinghdang@gmail.com>
2020-03-12 19:28:25 +0530

    Working on G0-5144

--- apps/web-giddh-admin/src/app/subscription/components/update-transactions/update-transactions.component.ts
+++ apps/web-giddh-admin/src/app/subscription/components/update-transactions/update-transactions.component.ts
@@ -1,17 +1,56 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { IOption } from '../../../theme/ng-select/ng-select';
+import { UserService } from '../../../services/user.service';
+import { PAGINATION_COUNT } from '../../../modules/modules/api-modules/subscription';
+
 @Component({
     selector: 'app-update-transactions',
     templateUrl: './update-transactions.component.html',
     styleUrls: ['./update-transactions.component.scss']
 })
-export class UpdateTransactionsComponent implements OnInit {
-  
-  public createPlanRequest: any = {};
-  public durationUnits: IOption[] = [{ label: 'YEAR', value: 'YEAR' }, { label: 'MONTH', value: 'MONTH' }, { label: 'WEEK', value: 'WEEK' }, { label: 'DAY', value: 'DAY' }];
 
-    constructor() { }
+export class UpdateTransactionsComponent implements OnInit {
+    @Output() public hideUpdateTransactionPopup: EventEmitter<boolean> = new EventEmitter(true);
+    @Input() public subscriptionId: string = '';
+
+    public transactionActions: IOption[] = [{ label: 'Add', value: 'add' }, { label: 'Deduct', value: 'deduct' }];
+    public getUserListRequest: any = {
+        sortBy: '',
+        sortType: '',
+        count: PAGINATION_COUNT,
+        page: 1
+    };
+    public getUserListPostRequest: any = {};
+    public subscribedUsers: any[] = [];
+    public updateTransactions: any = {};
+
+    constructor(private userService: UserService) { }
 
     ngOnInit() {
+        if(this.subscriptionId) {
+            this.getAllUserData();
+        } else {
+            this.closePopup();
+        }
+    }
+
+    public closePopup() {
+        this.hideUpdateTransactionPopup.emit();
+    }
+
+    /**
+     * This function is used to get user list
+     *
+     * @memberof UserListComponent
+     */
+    public getAllUserData() {
+        this.getUserListPostRequest = {subscriptionId: this.subscriptionId};
+        this.userService.getAllSubscriptionsByUser(this.getUserListRequest, this.getUserListPostRequest).subscribe(res => {
+            if (res.status === 'success' && res.body && res.body.results) {
+                this.subscribedUsers = res.body.results;
+            } else {
+                this.subscribedUsers = [];
+            }
+        });
     }
 }
