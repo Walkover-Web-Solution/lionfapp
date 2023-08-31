af3e5612c156508aadaa1dbdf7c3670827bafb2a
ravindersingh <ravinder@walkover.in>
2023-08-31 11:01:29 +0530

    Ravinder | Test | Shift Giddh Admin API  to Proxy Server

--- apps/web-giddh-admin/src/assets/magic-link-assets/magic-link-script.js
+++ apps/web-giddh-admin/src/assets/magic-link-assets/magic-link-script.js
@@ -298,10 +298,10 @@ var app = new Vue({
       switch (window.location.hostname) {
         case 'localapp.giddh.com':
         case 'dev.giddh.com':
-          apiBaseUrl = 'https://apitest.giddh.com/';
+          apiBaseUrl = 'https://routes.msg91.com/api/proxy/117230/18uykxj1/';
           break;
         case 'vtest.giddh.com':
-          apiBaseUrl = 'https://apitest.giddh.com/';
+          apiBaseUrl = 'https://routes.msg91.com/api/proxy/117230/18uykxj1/';
           break;
         case 'stage.giddh.com':
           apiBaseUrl = 'http://spi.giddh.com/';
